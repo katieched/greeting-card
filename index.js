fc2a0382c150b6385ccb832303586e3744a22a2f
Katie Chedgzoy <katieched98@hotmail.co.uk>
2021-09-23 11:59:19 +0100

    display card message

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-function greetingCard() {
-    const body = document.querySelector('body');
-    let greetingMessage = document.getElementById('#greeting');
+const myForm = document.querySelector('#myForm');
 
-    console.log(greetingMessage);
+const theBody = document.querySelector('body');
 
-}
-
-const submitForm = document.querySelector('#submitForm');
-
-submitForm.addEventListener("submit", e => {
+myForm.addEventListener("submit", e => {
     e.preventDefault();
+    const greetingMessage = e.target.greeting.value;
+
+    const birthday = e.target.birthday.value;
+    const christmas = e.target.christmas.value;
+    const anniversary = e.target.anniversary.value;
+
+    const fullMessage = e.target.message.value;
+
+    
+    if(birthday === "on") {
+        theBody.textContent = `${greetingMessage}. Happy Birthday! ${fullMessage}`;
+    }
+    else if(christmas === "on") {
+        theBody.textContent = `${greetingMessage}. Merry Christmas! ${fullMessage}`;
+    }
+    else if(anniversary === "on") {
+        theBody.textContent = `${greetingMessage}. Happy Anniversary! ${fullMessage}`;
+    }
+    else {
+        theBody.textContent = `${greetingMessage}. ${fullMessage}`;
+    }
 })
 
-submitForm.addEventListener("sumbit",greetingCard )
+
