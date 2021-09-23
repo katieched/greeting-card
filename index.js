1ae0d66645f424c6a1722fb61116b338aabbdfb4
Katie Chedgzoy <katieched98@hotmail.co.uk>
2021-09-23 14:48:02 +0100

    fix checkboxes

--- index.js
+++ index.js
@@ -6,20 +6,19 @@ myForm.addEventListener("submit", e => {
     e.preventDefault();
     const greetingMessage = e.target.greeting.value;
 
-    const birthday = e.target.birthday.value;
-    const christmas = e.target.christmas.value;
-    const anniversary = e.target.anniversary.value;
+    const birthday = e.target.birthday.checked;
+    const christmas = e.target.christmas.checked;
+    const anniversary = e.target.anniversary.checked;
 
     const fullMessage = e.target.message.value;
-
     
-    if(birthday === "on") {
+    if(birthday === true) {
         theBody.textContent = `${greetingMessage}. Happy Birthday! ${fullMessage}`;
     }
-    else if(christmas === "on") {
+    else if(christmas === true) {
         theBody.textContent = `${greetingMessage}. Merry Christmas! ${fullMessage}`;
     }
-    else if(anniversary === "on") {
+    else if(anniversary === true) {
         theBody.textContent = `${greetingMessage}. Happy Anniversary! ${fullMessage}`;
     }
     else {
